85ee707234c88cc6d029a56d295898d7c41da84e
Kie Ren <kieren.siew@gmail.com>
2024-10-07 16:05:26 +0800

    i added some buttons

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,8 @@ const Home = () => {
   useGSAP(() => {
     gsap.to("#logo", { opacity: 1, delay: 0.4, y: 0 });
     gsap.to("#title", { opacity: 1, y: 0, delay: 0.8 });
+    gsap.to("#button1", { opacity: 1, y: 0, delay: 1.1 });
+    gsap.to("#button2", { opacity: 1, y: 0, delay: 1.4 });
   }, []);
 
   return (
@@ -18,7 +20,7 @@ const Home = () => {
           unoptimized
           id="logo"
           src="/logo-nobg.png"
-          alt="EC3"
+          alt="Council Image"
           className="w-1/5 opacity-0 -translate-y-20"
           height={100}
           width={100}
@@ -26,10 +28,21 @@ const Home = () => {
 
         <h1
           className="text-6xl font-bold shiny-text max-w-[700px] pt-7 opacity-0 translate-y-20"
-          id="title"
-        >
+          id="title">
           {getCouncilName()}
         </h1>
+        <b>
+          <button
+            className="bg-gray-900 rounded-lg pl-5 pr-5 pt-2 pb-2 shiny-blue text-2xl m-5 mt-7 opacity-0 translate-y-10"
+            id="button1">
+            About us
+          </button>
+          <button
+            className="bg-gray-900 rounded-lg pl-5 pr-5 pt-2 pb-2 shiny-blue text-2xl m-5 mt-7 opacity-0 translate-y-10"
+            id="button2">
+            Initiatives
+          </button>
+        </b>
       </main>
     </div>
   );
